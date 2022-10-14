3cc73c15c4558b5fe30b90ddd72b28d1400fc764
mikemella <mike.mella227@gmail.com>
2022-10-14 17:51:34 +0000

    script not sorking

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
-console.log("in script");
 
-document.getElementById("pictureSubmit").addEventListener("click", function(event) {
+
+document.getElementById("pictureButton").addEventListener("click", function(event) {
+    console.log("in script");
     event.preventDefault();
-    const value = document.getElementById("weatherInput").value;
-    if (value === "")
-        return;
-    console.log(value);
+
+
     //Today's weather
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=61e624ee08da03cff27ece7209e66a49";
+    // const url = "https://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=61e624ee08da03cff27ece7209e66a49";
+    const url = "https://random.dog/woof.json"
     fetch(url)
         .then(function(response) {
             return response.json();
         }).then(function(json) {
             console.log(json)
-            let results = "";
-            results += '<h2>Weather in ' + json.name + " Today </h2>";
-            for (let i = 0; i < json.weather.length; i++) {
-                results += '<img src="https://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-            }
-            results += '<h2>' + json.main.temp + " &deg;F</h2>"
-            results += "<p>"
-            for (let i = 0; i < json.weather.length; i++) {
-                results += json.weather[i].description
-                if (i !== json.weather.length - 1)
-                    results += ", "
-            }
-            
-            results += '<p> Max Temp: <b>' + json.main.temp_max + "</b>&deg;F</p>";
-            results += '<p> Min Temp: <b>' + json.main.temp_min + "</b>&deg;F</p>";
-            results += '<p> Wind speed: <b>' + json.wind.speed + "</b> mph</p>";
-            // results += "</p>";
-            document.getElementById("weatherResults").innerHTML = results;
+        
+        let results = "";
+        // results += '<img src =' + json.url + "/>";
+        
+        results += '<video width="320" height="240" controls>';
+        results += '<source src =' + json.url + ">";
+        results += '</video>';
+        
+        
+        
+            document.getElementById("pictureResults").innerHTML = results;
         });
 
 });
