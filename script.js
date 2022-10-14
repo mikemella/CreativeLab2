704f2bd5b7827a21a330bbcef863d608cf196491
mikemella <mike.mella227@gmail.com>
2022-10-14 20:14:44 +0000

    almost done

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-
-
+let results = "";
 document.getElementById("pictureButton").addEventListener("click", function(event) {
-    console.log("in script");
+
     event.preventDefault();
 
 
     //Today's weather
     // const url = "https://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=61e624ee08da03cff27ece7209e66a49";
-    const url = "https://random.dog/woof.json"
-    fetch(url)
-        .then(function(response) {
-            return response.json();
-        }).then(function(json) {
-            console.log(json)
-        
-        let results = "";
-        // results += '<img src =' + json.url + "/>";
-        
-        results += '<video width="320" height="240" controls>';
-        results += '<source src =' + json.url + ">";
-        results += '</video>';
-        
-        
-        
-            document.getElementById("pictureResults").innerHTML = results;
-        });
-
+    for (let i = 0; i < 20; i++) {
+        const link = "https://random.dog/woof.json"
+        fetch(link)
+            .then(function(response) {
+                return response.json();
+            }).then(function(json) {
+                
+                if(json.url[json.url.length-1] != '4'){
+                    let int = json.url[json.url.length-1]
+                    console.log(json.url[json.url.length-1]);
+                    console.log(json.url);
+                results += '<img src =' + json.url + ">";
+                }
+                // results += '<video src =' + json.url + ">";
+                document.getElementById("pictureResults").innerHTML = results;
+            });
+    }
 });
